2ac9740040640f764e76e645c0d240d58a122693
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-02-24 16:03:55 +0200

    changes

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ window.application = {
     timers: []
 }
 
-
 function renderStartButton(container) {
     const startButton = document.createElement('button');
     startButton.textContent = 'Start';
@@ -40,45 +39,26 @@ function renderStartScreen() {
 
     const content = document.createElement('div');
     content.classList.add('startScreen__content');
-    const labelOne = document.createElement('label');
-    labelOne.classList.add('startScreen__label');
-    labelOne.htmlFor = '1';
-    labelOne.textContent = '1';
-    const inputOne = document.createElement('input');
-    inputOne.classList.add('startScreen__input');
-    inputOne.id = '1';
-    inputOne.setAttribute('type', 'radio');
-    inputOne.setAttribute('name', 'difficulty');
-
-    const labelTwo = document.createElement('label');
-    labelTwo.classList.add('startScreen__label');
-    labelTwo.htmlFor = '2';
-    labelTwo.textContent = '2';
-    const inputTwo = document.createElement('input');
-    inputTwo.classList.add('startScreen__input');
-    inputTwo.id = '2';
-    inputTwo.setAttribute('type', 'radio');
-    inputTwo.setAttribute('name', 'difficulty');
-
-    const labelThree = document.createElement('label');
-    labelThree.classList.add('startScreen__label');
-    labelThree.htmlFor = '3';
-    labelThree.textContent = '3';
-    const inputThree = document.createElement('input');
-    inputThree.classList.add('startScreen__input');
-    inputThree.id = '3';
-    inputThree.setAttribute('type', 'radio');
-    inputThree.setAttribute('name', 'difficulty');
-
 
     app.appendChild(title);
-    app.appendChild(content);
-    content.appendChild(labelOne);
-    content.appendChild(inputOne);
-    content.appendChild(labelTwo);
-    content.appendChild(inputTwo);
-    content.appendChild(labelThree);
-    content.appendChild(inputThree);
+
+    for (let i = 1; i <= 3; i++) {
+        const label = document.createElement('label');
+        label.classList.add('startScreen__label');
+        label.htmlFor = i;
+        label.textContent = i;
+        const input = document.createElement('input');
+        input.classList.add('startScreen__input');
+        input.id = i;
+        input.setAttribute('type', 'radio');
+        input.setAttribute('name', 'difficulty');
+
+        app.appendChild(content);
+        content.appendChild(label);
+        content.appendChild(input);
+    }
+
+
     window.application.renderBlock('start-button', app);
 }
 
