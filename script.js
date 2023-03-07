143b63542397149b85ae1c2aff6f3f896befce7e
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-03-08 01:05:18 +0200

    card render screen

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ window.application = {
     token: {},
     id: {},
     screens: {
-        start: renderStartScreen
+        start: renderStartScreen,
+        game: renderGameScreen
     },
     renderScreen: function (screenName) {
         this.screens[screenName]();
@@ -21,16 +22,21 @@ function renderStartButton(container) {
     startButton.textContent = 'Start';
     startButton.classList.add('startScreen__button');
 
-
     startButton.addEventListener('click', () => {
         window.application.level = document.querySelector('input[name="difficulty"]:checked').id;
-        if (window.application.level === 1) {
-            console.log(`easy one`);
-        } else if (window.application.level === 2) {
-            console.log(`medium one`);
-        } else if (window.application.level === 3) {
-            console.log(`hard one`);
+
+        let numCards;
+        if (window.application.level === '1') {
+            numCards = numCardsEasy;
+        } else if (window.application.level === '2') {
+            numCards = numCardsMedium;
+        } else if (window.application.level === '3') {
+            numCards = numCardsDifficult;
         }
+
+        const cards = generateCards(numCards);
+
+        renderGameScreen(cards);
     });
 
     container.appendChild(startButton);
@@ -41,6 +47,9 @@ function renderStartScreen() {
     const app = document.querySelector('.app');
     app.textContent = '';
 
+    const background = document.createElement('div');
+    background.classList.add('startScreen__background');
+
     const title = document.createElement('h1');
     title.textContent = 'Choose difficulty';
     title.classList.add('startScreen__title');
@@ -48,7 +57,8 @@ function renderStartScreen() {
     const content = document.createElement('div');
     content.classList.add('startScreen__content');
 
-    app.appendChild(title);
+    app.appendChild(background);
+    background.appendChild(title);
 
     for (let i = 1; i <= 3; i++) {
         const label = document.createElement('label');
@@ -61,13 +71,83 @@ function renderStartScreen() {
         input.setAttribute('type', 'radio');
         input.setAttribute('name', 'difficulty');
 
-        app.appendChild(content);
-        content.appendChild(label);
+        background.appendChild(content);
         content.appendChild(input);
+        content.appendChild(label);
+
     }
 
 
-    window.application.renderBlock('start-button', app);
+    window.application.renderBlock('start-button', background);
 }
 
 window.application.renderScreen('start');
+
+const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+const ranks = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+
+const numCardsEasy = 6;
+const numCardsMedium = 12;
+const numCardsDifficult = 20;
+
+
+function generateCards(numCards) {
+    const cards = [];
+
+    for (let i = 0; i < numCards / 2; i++) {
+        const suit = suits[Math.floor(Math.random() * suits.length)];
+        const rank = ranks[Math.floor(Math.random() * ranks.length)];
+        cards.push({ suit, rank });
+        cards.push({ suit, rank });
+    }
+
+    for (let i = cards.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [cards[i], cards[j]] = [cards[j], cards[i]];
+    }
+
+    return cards;
+};
+
+function renderGameScreen(cards) {
+    const app = document.querySelector('.app');
+    app.textContent = '';
+
+    const board = document.createElement('div');
+    board.classList.add('gameScreen__board');
+
+    for (let i = 0; i < cards.length; i++) {
+        const card = document.createElement('div');
+        card.classList.add('gameScreen__card');
+        card.dataset.index = i;
+
+        const front = document.createElement('div');
+        front.classList.add('gameScreen__front');
+        front.classList.add(cards[i].suit + cards[i].rank);
+
+        const back = document.createElement('div');
+        back.classList.add('gameScreen__back');
+
+        card.appendChild(front);
+        card.appendChild(back);
+        board.appendChild(card);
+    }
+    app.appendChild(board);
+}
+
+let choosenCard = null;
+let numMatchedCards = 0;
+
+const createdCards = document.querySelectorAll('.card');
+
+createdCards.forEach((card) => {
+
+    card.addEventListener('click', () => {
+
+        console.log(`you clicked on a card`);
+
+    })
+
+});
+
+
