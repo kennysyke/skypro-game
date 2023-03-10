8ebf3e867b3925b754de9c61417caf05c883ca78
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-03-10 11:47:42 +0200

    changes in flipping function

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 window.application = {
     blocks: {
-        'start-button': renderStartButton
+        'start-button': renderStartButton,
+        'restart-button': renderRestartButton,
     },
     token: {},
     id: {},
     screens: {
         start: renderStartScreen,
-        game: renderGameScreen
+        game: renderGameScreen,
     },
     renderScreen: function (screenName) {
         this.screens[screenName]();
@@ -14,8 +15,8 @@ window.application = {
     renderBlock: function (blockName, container) {
         this.blocks[blockName](container);
     },
-    level: []
-}
+    level: [],
+};
 
 function renderStartButton(container) {
     const startButton = document.createElement('button');
@@ -23,7 +24,9 @@ function renderStartButton(container) {
     startButton.classList.add('startScreen__button');
 
     startButton.addEventListener('click', () => {
-        window.application.level = document.querySelector('input[name="difficulty"]:checked').id;
+        window.application.level = document.querySelector(
+            'input[name="difficulty"]:checked'
+        ).id;
 
         let numCards;
         if (window.application.level === '1') {
@@ -41,7 +44,6 @@ function renderStartButton(container) {
 
     container.appendChild(startButton);
 }
-
 
 function renderStartScreen() {
     const app = document.querySelector('.app');
@@ -74,9 +76,7 @@ function renderStartScreen() {
         background.appendChild(content);
         content.appendChild(input);
         content.appendChild(label);
-
     }
-
 
     window.application.renderBlock('start-button', background);
 }
@@ -89,7 +89,6 @@ const ranks = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 const numCardsEasy = 6;
 const numCardsMedium = 12;
 const numCardsDifficult = 20;
-
 
 function generateCards(numCards) {
     const cards = [];
@@ -107,11 +106,37 @@ function generateCards(numCards) {
     }
 
     return cards;
-};
+}
+
+function renderRestartButton(container) {
+    const restartButton = document.createElement('button');
+    restartButton.textContent = 'Restart';
+    restartButton.classList.add('gameScreen__button');
+
+    restartButton.addEventListener('click', () => {
+        let numCards;
+        if (window.application.level === '1') {
+            numCards = numCardsEasy;
+        } else if (window.application.level === '2') {
+            numCards = numCardsMedium;
+        } else if (window.application.level === '3') {
+            numCards = numCardsDifficult;
+        }
+
+        const cards = generateCards(numCards);
+
+        renderGameScreen(cards);
+    });
+
+    container.appendChild(restartButton);
+}
 
 function renderGameScreen(cards) {
     const app = document.querySelector('.app');
     app.textContent = '';
+
+    const topboard = document.createElement('div');
+    topboard.classList.add('gameScreen__topboard');
 
     const board = document.createElement('div');
     board.classList.add('gameScreen__board');
@@ -120,6 +145,8 @@ function renderGameScreen(cards) {
         const card = document.createElement('div');
         card.classList.add('gameScreen__card');
         card.dataset.index = i;
+        card.dataset.suit = cards[i].suit;
+        card.dataset.rank = cards[i].rank;
 
         const front = document.createElement('div');
         front.classList.add('gameScreen__front');
@@ -131,23 +158,68 @@ function renderGameScreen(cards) {
         card.appendChild(front);
         card.appendChild(back);
         board.appendChild(card);
+
+        setTimeout(() => {
+            card.classList.add('flipped');
+        }, 5000);
     }
+
+    app.appendChild(topboard);
     app.appendChild(board);
+
+    const timerCount = document.createElement('div');
+    timerCount.classList.add('gameScreen__timer');
+    let time = 0;
+    const timerInterval = setInterval(() => {
+        time++;
+        timerCount.textContent = `Time: ${time} seconds`;
+    }, 1000);
+    topboard.appendChild(timerCount);
+    window.application.renderBlock('restart-button', topboard);
+
+    const endTimer = () => {
+        clearInterval(timerInterval);
+    };
+
+    let flippedCard = null;
+    let matchedCards = 0;
+
+    function flipCard(card) {
+        console.log(card);
+        console.log(flippedCard);
+        console.log(this);
+        if (flippedCard === null) {
+            this.classList.remove('flipped');
+            flippedCard = this;
+            console.log(flippedCard);
+        } else if (this === flippedCard) {
+            return;
+        } else {
+            this.classList.remove('flipped');
+            if (
+                flippedCard.dataset.rank === this.dataset.rank &&
+                flippedCard.dataset.suit === this.dataset.suit
+            ) {
+                matchedCards++;
+                flippedCard = null;
+                if (matchedCards === cards.length / 2) {
+                    endTimer();
+                    console.log(`you have won`);
+                }
+            } else {
+                console.log(`game over`);
+            }
+        }
+    }
+
+    const createdCards = document.querySelectorAll('.gameScreen__card');
+
+    console.log(createdCards);
+
+    createdCards.forEach((card) => {
+        card.addEventListener('click', () => {
+            flipCard(card);
+            card.classList.toggle('flipped');
+        });
+    });
 }
-
-let choosenCard = null;
-let numMatchedCards = 0;
-
-const createdCards = document.querySelectorAll('.card');
-
-createdCards.forEach((card) => {
-
-    card.addEventListener('click', () => {
-
-        console.log(`you clicked on a card`);
-
-    })
-
-});
-
-
