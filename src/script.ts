35a0f817770594014a69cfef6213f491997a9352
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-03-17 21:06:21 +0200

    ts2.0

--- src/script.ts
+++ src/script.ts
@@ -1,6 +1,42 @@
 import '/static/style.css';
 import '/static/cards.css';
 
+interface Card {
+    rank: string;
+    suit: string;
+}
+
+type Level = '1' | '2' | '3';
+
+declare global {
+    interface Window {
+        application: {
+            blocks: {
+                [key: string]: (container: HTMLElement) => void;
+            };
+            token: any;
+            id: any;
+            screens: {
+                [key: string]: () => void;
+            };
+            renderScreen: (screenName: string) => void;
+            renderBlock: (blockName: string, container: HTMLElement) => void;
+            level: Level | null;
+        };
+    }
+}
+
+// interface Application {
+//     blocks: Record<string, (container: HTMLElement) => void>;
+//     token: {};
+//     id: {};
+//     screens: Record<string, () => void>;
+//     renderScreen: (screenName: string) => void;
+//     renderBlock: (blockName: string, container: HTMLElement) => void;
+//     renderGameScreen: (cards: Card[]) => void;
+//     level: Level | null;
+// }
+
 window.application = {
     blocks: {
         'start-button': renderStartButton,
@@ -12,26 +48,26 @@ window.application = {
         start: renderStartScreen,
         game: renderGameScreen,
     },
-    renderScreen: function (screenName) {
+    renderScreen: function (screenName: string) {
         this.screens[screenName]();
     },
-    renderBlock: function (blockName, container) {
+    renderBlock: function (blockName: string, container: HTMLElement) {
         this.blocks[blockName](container);
     },
-    level: [],
+    level: null,
 };
 
-function renderStartButton(container) {
+function renderStartButton(container: HTMLElement): void {
     const startButton = document.createElement('button');
     startButton.textContent = 'Start';
     startButton.classList.add('startScreen__button');
 
     startButton.addEventListener('click', () => {
-        window.application.level = document.querySelector(
+        window.application.level = document.querySelector<HTMLInputElement>(
             'input[name="difficulty"]:checked'
-        ).id;
+        )?.id;
 
-        let numCards;
+        let numCards: number;
         if (window.application.level === '1') {
             numCards = numCardsEasy;
         } else if (window.application.level === '2') {
@@ -49,7 +85,7 @@ function renderStartButton(container) {
 }
 
 function renderStartScreen() {
-    const app = document.querySelector('.app');
+    const app = document.querySelector('.app')!;
     app.textContent = '';
 
     const background = document.createElement('div');
@@ -68,11 +104,11 @@ function renderStartScreen() {
     for (let i = 1; i <= 3; i++) {
         const label = document.createElement('label');
         label.classList.add('startScreen__label');
-        label.htmlFor = i;
-        label.textContent = i;
+        label.htmlFor = i.toString();
+        label.textContent = i.toString();
         const input = document.createElement('input');
         input.classList.add('startScreen__input');
-        input.id = i;
+        input.id = i.toString();
         input.setAttribute('type', 'radio');
         input.setAttribute('name', 'difficulty');
 
@@ -84,40 +120,47 @@ function renderStartScreen() {
     window.application.renderBlock('start-button', background);
 }
 
-window.application.renderScreen('start');
-
-const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
-const ranks = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+renderStartScreen();
 
 const numCardsEasy = 6;
 const numCardsMedium = 12;
 const numCardsDifficult = 18;
 
-function generateCards(numCards) {
-    const cards = [];
+function generateCards(numCards: number): Card[] {
+    const ranks = ['A', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+    const suits = ['hearts', 'diamonds', 'clubs', 'spades'];
+    const cards: Card[] = [];
 
     for (let i = 0; i < numCards / 2; i++) {
-        const suit = suits[Math.floor(Math.random() * suits.length)];
-        const rank = ranks[Math.floor(Math.random() * ranks.length)];
-        cards.push({ suit, rank });
-        cards.push({ suit, rank });
+        const rankIndex = Math.floor(Math.random() * ranks.length);
+        const suitIndex = Math.floor(Math.random() * suits.length);
+
+        const card: Card = {
+            suit: suits[suitIndex],
+            rank: ranks[rankIndex],
+        };
+
+        const matchingCard: Card = {
+            suit: suits[suitIndex],
+            rank: ranks[rankIndex],
+        };
+
+        if (!cards.some((c) => c.rank === card.rank && c.suit === card.suit)) {
+            cards.push(card);
+            cards.push(matchingCard);
+        }
     }
 
-    for (let i = cards.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [cards[i], cards[j]] = [cards[j], cards[i]];
-    }
-
-    return cards;
+    return cards.sort(() => Math.random() - 0.5);
 }
 
-function renderRestartButton(container) {
+function renderRestartButton(container: HTMLElement): void {
     const restartButton = document.createElement('button');
     restartButton.textContent = 'Restart';
     restartButton.classList.add('gameScreen__button');
 
     restartButton.addEventListener('click', () => {
-        let numCards;
+        let numCards: number;
         if (window.application.level === '1') {
             numCards = numCardsEasy;
         } else if (window.application.level === '2') {
@@ -134,8 +177,8 @@ function renderRestartButton(container) {
     container.appendChild(restartButton);
 }
 
-function renderGameScreen(cards) {
-    const app = document.querySelector('.app');
+function renderGameScreen(cards: { suit: string; rank: string }[]) {
+    const app = document.querySelector('.app') as Element;
     app.textContent = '';
 
     const topboard = document.createElement('div');
@@ -149,7 +192,7 @@ function renderGameScreen(cards) {
     for (let i = 0; i < cards.length; i++) {
         const card = document.createElement('div');
         card.classList.add('gameScreen__card');
-        card.dataset.index = i;
+        card.dataset.index = i.toString();
         card.dataset.suit = cards[i].suit;
         card.dataset.rank = cards[i].rank;
 
@@ -192,12 +235,18 @@ function renderGameScreen(cards) {
         clearInterval(timerInterval);
     };
 
-    function showModal(won, timeTaken) {
-        const overlay = document.querySelector('.overlay');
-        const modalHeader = document.querySelector('.modal-header-text');
-        const modalImage = document.querySelector('.modal-image');
-        const modalTimeTaken = document.querySelector('.modal-time-taken');
-        const modalTime = document.querySelector('.modal-time');
+    function showModal(won: boolean, timeTaken: number): void {
+        const overlay = document.querySelector('.overlay') as HTMLElement;
+        const modalHeader = document.querySelector(
+            '.modal-header-text'
+        ) as HTMLElement;
+        const modalImage = document.querySelector(
+            '.modal-image'
+        ) as HTMLImageElement;
+        const modalTimeTaken = document.querySelector(
+            '.modal-time-taken'
+        ) as HTMLElement;
+        const modalTime = document.querySelector('.modal-time') as HTMLElement;
 
         // Set modal header text and image based on whether the player won or lost
         if (won) {
@@ -220,26 +269,25 @@ function renderGameScreen(cards) {
         overlay.style.display = 'block';
 
         // Set up event listener for restart button
-        const restartButton = document.querySelector('.modal-restart-button');
+        const restartButton = document.querySelector(
+            '.modal-restart-button'
+        ) as HTMLElement;
         restartButton.addEventListener('click', () => {
             overlay.style.display = 'none';
-            window.application.renderScreen('start');
+            renderStartScreen();
         });
 
         // Set up event listener for close button
-        const closeButton = document.querySelector('.close');
+        const closeButton = document.querySelector('.close') as HTMLElement;
         closeButton.addEventListener('click', () => {
             overlay.style.display = 'none';
         });
     }
 
-    let flippedCard = null;
-    let matchedCards = 0;
+    let flippedCard: HTMLElement | null = null;
+    let matchedCards: number = 0;
 
-    function flipCard(card) {
-        console.log(card);
-        console.log(flippedCard);
-        console.log(this);
+    function flipCard(this: HTMLElement) {
         if (flippedCard === null) {
             this.classList.remove('flipped');
             flippedCard = this;
@@ -248,26 +296,30 @@ function renderGameScreen(cards) {
             return;
         } else {
             this.classList.remove('flipped');
+            const flippedCardRank = flippedCard.dataset.rank;
+            const flippedCardSuit = flippedCard.dataset.suit;
+            const thisCardRank = this.dataset.rank;
+            const thisCardSuit = this.dataset.suit;
+
             if (
-                flippedCard.dataset.rank === this.dataset.rank &&
-                flippedCard.dataset.suit === this.dataset.suit
+                flippedCardRank === thisCardRank &&
+                flippedCardSuit === thisCardSuit
             ) {
                 matchedCards++;
                 flippedCard = null;
                 if (matchedCards === cards.length / 2) {
                     endTimer();
                     showModal(true, time);
-                    console.log(`you have won`);
                 }
             } else {
                 endTimer();
                 showModal(false, time);
-                console.log(`game over`);
             }
         }
     }
 
-    const createdCards = document.querySelectorAll('.gameScreen__card');
+    const createdCards: NodeListOf<HTMLElement> =
+        document.querySelectorAll('.gameScreen__card');
 
     console.log(createdCards);
 
