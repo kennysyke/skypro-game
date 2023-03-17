d942091b5ff34397591a3c88ca3e8f5598b861dc
Aleksandra Schmidt <itsgnolio@gmail.com>
2023-03-17 20:12:43 +0200

    typescript1.0

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const mode =
+    process.env.NODE_ENV === 'production' ? 'production' : 'development';
+
+module.exports = {
+    entry: './src/script.js',
+    mode,
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+    optimization: {
+        minimizer: ['...', new CssMinimizerPlugin()],
+    },
+    devtool:
+        process.env.NODE_ENV === 'production'
+            ? 'hidden-source-map'
+            : 'source-map',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'bundle.js',
+        clean: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+        }),
+        new CopyPlugin({
+            patterns: [{ from: 'static', to: 'static' }],
+        }),
+    ],
+};
